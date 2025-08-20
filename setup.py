dba3a0b69698b8b944efa4c83aae74a5ec0b5689
Ridham <ridhampuri42@gmail.com>
2025-08-20 16:15:48 +0200

    one step before running applicaton

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="AgentForge",
+    name="MULTI AI AGENT",
     version="0.1",
     author="Ridham Puri",
     packages=find_packages(),
